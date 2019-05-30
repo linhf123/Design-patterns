dd2aabe229f7a061a1b598eedc80be0739b208ac
@linhf <32009993+Overcase@users.noreply.github.com>
2019-05-30 16:08:36 +0800

    Update create.js

--- design/lesson1/code/create.js
+++ design/lesson1/code/create.js
@@ -1,4 +1,4 @@
-// Js get new object for use prototype clone ohter object
+// Js get new object for use prototype clone other object
 // Js 通过克隆其他对象 得到一个新的对象
 
 Object.create = Object.create || function (obj) {
